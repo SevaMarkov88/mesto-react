0551ef2581f5f85c44bf44fbd9a0404715759f95
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-01-22 15:54:50 +0300

    new version

--- src/components/PopupSubmitDelete.js
+++ src/components/PopupSubmitDelete.js
@@ -1,11 +1,10 @@
 import PopupWithForm from "./PopupWithForm";
 
-function PopupSubmitDelete() {
+function PopupSubmitDelete(props) {
   return (
-    <PopupWithForm name="accept-delete" title="Вы уверены?">
-      <form action="#" className="popup__form" noValidate>
-        <button className="popup__button" type="submit" aria-label="Сохранить данные">Да</button>
-      </form>
+    <PopupWithForm name="accept-delete" title="Вы уверены?" btn="Да" formName="delete"
+                   isOpen={props.isOpen && "popup_opened"}
+                   onClose={props.onclose}>
     </PopupWithForm>
   )
 }
