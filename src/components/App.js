8178e26f6d63753cb70d5bd0679c362bf062b949
Seva Markov <vsevolodmarkoff@yandex.ru>
2022-01-30 13:11:23 +0300

    All ready

--- src/components/App.js
+++ src/components/App.js
@@ -46,8 +46,8 @@ function App() {
 
   function handleCardDelete(card) {
     api.deleteCard(card._id)
-      .then((newCard) => {
-        const newCards = cards.filter((c) => c._id === card._id ? newCard : c);
+      .then(() => {
+        const newCards = cards.filter((c) => c._id !== card._id);
         setCards(newCards);
       })
       .catch((err) => console.log(err))
